32fd3744b56e4548fe2f2265617a3cceff9fa11d
Ashutosh Tiwari <ashutosh@Apples-MacBook-Pro.local>
2024-04-22 12:43:47 +0530

    add hero component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import FeaturePosts from "@/components/home-page/feature-posts";
+import Hero from "@/components/home-page/hero";
+
 export default function Home() {
   return (
     <>
-      <h1>Home Page</h1>
+      <Hero />
+      <FeaturePosts />
     </>
   );
 }
