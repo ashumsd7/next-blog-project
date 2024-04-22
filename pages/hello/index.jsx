c87afd8e61f7c7250e562efed3fc391bd0419fb5
Ashutosh Tiwari <ashutosh@Apples-MacBook-Pro.local>
2024-04-22 12:14:21 +0530

    WIP: Add pages

---
+++ pages/hello/index.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+function index() {
+  return (
+    <div>
+      <Link href="/">Hey</Link>
+    </div>
+  );
+}
+
+export default index;
---
+++ pages/posts/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AllPosts() {
+  return <div>AllPosts</div>;
+}
+
+export default AllPosts;
