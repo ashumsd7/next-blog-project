d8dfd6c60f55cce540ed9b58a7ee0f332aea7e10
Ashutosh Tiwari <ashutosh@Apples-MacBook-Pro.local>
2024-04-23 12:54:13 +0530

    add Head

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import Layout from "@/components/layout/layout";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Hey bloggers</title>
+        <meta name="viewport content=etc...." />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
