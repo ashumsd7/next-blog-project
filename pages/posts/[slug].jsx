e7bfb025a2605474349ba4704b0acaa09ac977a0
Ashutosh Tiwari <ashutosh@Apples-MacBook-Pro.local>
2024-04-23 11:12:34 +0530

    use getstaticppaths

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -1,6 +1,35 @@
 import PostContent from "@/components/posts/post-detail/post-content";
+import { getPostData, getPostFiles } from "@/lib/posts-util";
 import React from "react";
 
-export default function EachPost() {
-  return <PostContent></PostContent>;
+export default function EachPost(props) {
+  return <PostContent post={props.post}></PostContent>;
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const post = getPostData(slug);
+  return {
+    props: {
+      post: post,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const fileNames = getPostFiles();
+  const slugs = fileNames.map((file) => file.replace(/\.md$/, ""));
+  return {
+    paths: slugs?.map((slug) => {
+      return {
+        params: {
+          slug: slug,
+        },
+      };
+    }),
+    fallback: false,
+  };
 }
