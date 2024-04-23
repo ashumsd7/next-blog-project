0d0501a6e54cb60519faffccea72cd54b63f53b0
Ashutosh Tiwari <ashutosh@Apples-MacBook-Pro.local>
2024-04-23 12:05:51 +0530

    wip contatc form

---
+++ components/contact/contact-form.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import classes from "./contact-form.module.css";
+function ContactForm() {
+  return (
+    <section>
+      <h1>How can I help you</h1>
+      <form className={classes.form}>
+        <div className={classes.controls}>
+          <div className={classes.control}>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" />
+          </div>
+        </div>
+        <div className={classes.control}>
+          <div className={classes.control}>
+            <label htmlFor="email">Your Email</label>
+            <input type="email" id="email" />
+          </div>
+        </div>
+        <div className={classes.control}>
+          <div className={classes.control}>
+            <label htmlFor="message">Message</label>
+            <textarea id="message"></textarea>
+          </div>
+        </div>
+        <div className={classes.actions}>
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default ContactForm;
