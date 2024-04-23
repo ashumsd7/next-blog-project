f5f953d81433c4df898d19ba0426d21098c1c7d0
Ashutosh Tiwari <ashutosh@Apples-MacBook-Pro.local>
2024-04-23 11:28:49 +0530

    use getstaticppaths

--- components/posts/post-detail/post-content.jsx
+++ components/posts/post-detail/post-content.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 // const DUMMY_POST = {
 //   title: "Getting Started With Next JS",
 //   slug: "getting-started-nex-js-2",
@@ -11,13 +12,26 @@ import ReactMarkdown from "react-markdown";
 //   image: "getting-started-next-js.png",
 //   content: `## Hello, world [hey](https://wwww.www.ww)`,
 // };
+
 function PostContent(props) {
+  const customRenders = {
+    image(image) {
+      return (
+        <Image
+          src={`/images/posts/${image.src}`}
+          width={"600"}
+          height={"300"}
+          alt={"image.alt"}
+        />
+      );
+    },
+  };
   const { post } = props;
   const imagePath = `/images/posts/${post.image}`;
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenders}>{post.content}</ReactMarkdown>
     </article>
   );
 }
